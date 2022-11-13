c9e8af1d938ffc117684a43ffd0c2824901fc87a
shotamishima <sm4mma@gmail.com>
2022-11-13 23:20:04 +0900

    Create client code

---
+++ src/main/java/in/tutorial/grpc/HelloWorldClient.java
@@ -0,0 +1,16 @@
+package in.tutorial.grpc;
+
+import javax.annotation.Nonnull;
+
+import io.grpc.Channel;
+
+public class HelloWorldClient {
+
+    public HelloWorldClient(Channel channel) {
+
+    }
+
+    public void greet(String string) {
+
+    }
+}
