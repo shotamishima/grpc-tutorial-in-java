251fd677855b38cfff48bbe7f3ab0b0a7dc8bc5d
shotamishima <sm4mma@gmail.com>
2022-11-16 00:38:59 +0900

    Make perfect hellow world client

--- src/main/java/in/tutorial/grpc/HelloWorldClient.java
+++ src/main/java/in/tutorial/grpc/HelloWorldClient.java
@@ -1,16 +1,64 @@
 package in.tutorial.grpc;
 
-import javax.annotation.Nonnull;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 
 public class HelloWorldClient {
 
+    private static final Logger logger = Logger.getLogger(HelloWorldClient.class.getName());
+
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
     public HelloWorldClient(Channel channel) {
+        // Channel is not shutted down automatically unlike managedchannel
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
 
     }
 
-    public void greet(String string) {
+    public void greet(String name) {
+        logger.info("Will try to greet " + name + " ...");
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+        HelloReply response;
+        try {
+            response = blockingStub.sayHello(request);
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return;
+        }
+        logger.info("Greeting: " + response.getMessage());
+    }
+
+    public static void main(String[] args) throws Exception {
+        String user = "World";
+        String target = "localhost:50051";
+
+        if (args.length > 0) {
+            if ("--help".equals(args[0])) {
+                System.err.println("Usafe: [name [target]]");
+                System.err.println("");
+                System.err.println("  name   The name you wish to be greeted by. Defaults to " + user);
+                System.err.println("  target The server to connect to. Defaults to " + target);
+            }
+            user = args[0];
+        }
+        if (args.length > 1) {
+            target = args[1];
+        }
+
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+
+        try {
+            HelloWorldClient client = new HelloWorldClient(channel);
+            client.greet(user);
+        } finally {
+            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+        }
 
     }
 }
