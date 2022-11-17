e55febb87a8e3f50b62f696ab1b00042850f76ce
shotamishima <sm4mma@gmail.com>
2022-11-17 12:00:16 +0900

    Add server script

--- src/main/java/in/tutorial/grpc/HelloWorldServer.java
+++ src/main/java/in/tutorial/grpc/HelloWorldServer.java
@@ -1,8 +1,65 @@
 package in.tutorial.grpc;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 
+/**
+ * Server that manages startup/shutdown of a Greeter server.
+ */
 public class HelloWorldServer {
+
+    private Server server;
+
+    private void start() throws IOException {
+        int port = 50051;
+        server = ServerBuilder
+                .forPort(port)
+                .addService(new GreeterImpl())
+                .build()
+                .start();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                try {
+                    // how to access class method
+                    HelloWorldServer.this.stop();
+                } catch (InterruptedException e) {
+                    e.printStackTrace(System.err);
+                }
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    private void stop() throws InterruptedException {
+        if (server != null) {
+            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+        }
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon
+     * threads.
+     * 
+     * @throws InterruptedException
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final HelloWorldServer server = new HelloWorldServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
+
     static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
 
         @Override
