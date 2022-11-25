b36e3422810d9f05a4507c3fe613bdbee4f798d6
shotamishima <sm4mma@gmail.com>
2022-11-25 20:01:51 +0900

    Update server-side streaming client

--- src/main/java/in/tutorial/grpc/HelloServerStreamingClient.java
+++ src/main/java/in/tutorial/grpc/HelloServerStreamingClient.java
@@ -1,5 +1,13 @@
 package in.tutorial.grpc;
 
+import io.grpc.Channel;
+
 public class HelloServerStreamingClient {
+
+    private final GreeterGrpc.GreeterBlockingStub stub;
+
+    public HelloServerStreamingClient(Channel channel) {
+        stub = GreeterGrpc.newBlockingStub(channel);
+    }
 
 }
--- src/test/java/in/tutorial/grpc/HelloServerStreamingClientTest.java
+++ src/test/java/in/tutorial/grpc/HelloServerStreamingClientTest.java
@@ -1,5 +1,53 @@
 package in.tutorial.grpc;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import java.io.IOException;
+
+import static org.mockito.AdditionalAnswers.delegatesTo;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import io.grpc.ManagedChannel;
+import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.inprocess.InProcessServerBuilder;
+import io.grpc.testing.GrpcCleanupRule;
+
 public class HelloServerStreamingClientTest {
+
+    @Rule
+    public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
+
+    // mock object can use methods original class has by delegating
+    private final GreeterGrpc.GreeterImplBase serviceImpl = mock(GreeterGrpc.GreeterImplBase.class, delegatesTo(
+            new GreeterGrpc.GreeterImplBase() {
+            }));
+
+    private HelloServerStreamingClient client;
+
+    @Before
+    public void setUp() throws IOException {
+        String serverName = InProcessServerBuilder.generateName();
+
+        // Create server and register for automatic graceful shutdown
+        grpcCleanup.register(
+                InProcessServerBuilder.forName(serverName).directExecutor().addService(serviceImpl).build().start());
+
+        // Create channel to be referenced by client
+        ManagedChannel channel = grpcCleanup.register(
+                InProcessChannelBuilder.forName(serverName).directExecutor().build());
+
+        // Create client
+        client = new HelloServerStreamingClient(channel);
+    }
+
+    @Test
+    public void test() {
+        client.greet("test");
+        assertEquals("test", a);
+    }
 
 }
