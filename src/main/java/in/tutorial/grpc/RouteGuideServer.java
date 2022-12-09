647295a2903b3f610c1f13ea17c650dab5cfd12c
shotamishima <sm4mma@gmail.com>
2022-12-09 09:02:25 +0900

    Add route guider server implement

---
+++ src/main/java/in/tutorial/grpc/RouteGuideServer.java
@@ -0,0 +1,116 @@
+package in.tutorial.grpc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import io.grpc.Grpc;
+import io.grpc.InsecureServerCredentials;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+public class RouteGuideServer {
+    private static final Logger logger = Logger.getLogger(RouteGuideServer.class.getName());
+
+    private final int port;
+    private final Server server;
+
+    public RouteGuideServer(ServerBuilder<?> serverBuilder, int port, Collection<Feature> features) {
+        this.port = port;
+        server = serverBuilder.addService(new RouteGuideService(features)).build();
+    }
+
+    /** Start serving request. */
+    public void start() throws IOException {
+        server.start();
+        logger.info("Server statrted, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                // Use stderr here since the logger may have been reset by its JVM shutdown
+                // hook.
+                System.err.println("*** shutting down gRPC server since JAM is shutting down");
+                try {
+                    RouteGuideServer.this.stop();
+                } catch (InterruptedException e) {
+                    e.printStackTrace(System.err);
+                }
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    /** Stop serving requests and shutdown resources. */
+    public void stop() throws InterruptedException {
+        if (server != null) {
+            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+        }
+    }
+
+    public static class RouteGuideService extends GreeterGrpc.GreeterImplBase {
+
+        private final Collection<Feature> features;
+        private final ConcurrentMap<Point, List<RouteNote>> routeNotes = new ConcurrentHashMap<Point, List<RouteNote>>();
+
+        public RouteGuideService(Collection<Feature> features) {
+            this.features = features;
+        }
+
+        /**
+         * Receives a stream of message/location pairs, and responds with a stream of
+         * all previous messages at each of thos locations.
+         * 
+         * @param responseObserver an observer to receive the stream of previous
+         *                         messages.
+         * @return an observer to handle requested message/location pairs.
+         */
+        @Override
+        public StreamObserver<RouteNote> routeChat(final StreamObserver<RouteNote> responseObserver) {
+            // StreamObserver is an interface, so it can be return without override methods.
+            return new StreamObserver<RouteNote>() {
+                @Override
+                public void onNext(RouteNote note) {
+                    List<RouteNote> notes = getOrCreateNotes(note.getLocation());
+
+                    // Respond with all previious notes at this location.
+                    for (RouteNote prevNote : notes.toArray(new RouteNote[0])) {
+                        responseObserver.onNext(prevNote);
+                    }
+
+                    // Now add the new note to the list
+                    notes.add(note);
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    logger.log(Level.WARNING, "routeChat cancelled");
+                }
+
+                @Override
+                public void onCompleted() {
+                    responseObserver.onCompleted();
+                }
+            };
+        }
+
+        /**
+         * Ger the notes list for the given location. If missing, create it.
+         * 
+         * @param location
+         * @return
+         */
+        private List<RouteNote> getOrCreateNotes(Point location) {
+            List<RouteNote> notes = Collections.synchronizedList(new ArrayList<RouteNote>());
+            List<RouteNote> prevNotes = routeNotes.putIfAbsent(location, notes);
+            return prevNotes != null ? prevNotes : notes;
+        }
+    }
+}
--- src/test/java/in/tutorial/grpc/RouteGuideServerTest.java
+++ src/test/java/in/tutorial/grpc/RouteGuideServerTest.java
@@ -1,10 +1,91 @@
 package in.tutorial.grpc;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.ArgumentCaptor;
 
+import com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse.Feature;
+
+import io.grpc.ManagedChannel;
+import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.inprocess.InProcessServerBuilder;
+import io.grpc.stub.StreamObserver;
+import io.grpc.testing.GrpcCleanupRule;
+
+@RunWith(JUnit4.class)
 public class RouteGuideServerTest {
-    @Test
-    public void test() {
 
+    @Rule
+    public final GrpcCleanupRule grpcCleanupRule = new GrpcCleanupRule();
+
+    private ManagedChannel inProcessChannel;
+    private Collection<in.tutorial.grpc.Feature> features;
+    private RouteGuideServer server;
+
+    @Before
+    public void setUp() throws IOException {
+        String serverName = InProcessServerBuilder.generateName();
+        features = new ArrayList<>();
+        // Generate server
+        server = new RouteGuideServer(InProcessServerBuilder.forName(serverName).directExecutor(), 0, features);
+        server.start();
+
+        // Generate client
+        inProcessChannel = grpcCleanupRule.register(
+                InProcessChannelBuilder.forName(serverName).directExecutor().build());
+    }
+
+    @Test
+    public void routeChatTest() {
+        Point p1 = Point.newBuilder().setLatitude(1).setLongitude(1).build();
+        Point p2 = Point.newBuilder().setLatitude(2).setLongitude(2).build();
+        RouteNote n1 = RouteNote.newBuilder().setLocation(p1).setMessage("m1").build();
+        RouteNote n2 = RouteNote.newBuilder().setLocation(p2).setMessage("m2").build();
+        RouteNote n3 = RouteNote.newBuilder().setLocation(p1).setMessage("m3").build();
+        RouteNote n4 = RouteNote.newBuilder().setLocation(p2).setMessage("m4").build();
+        RouteNote n5 = RouteNote.newBuilder().setLocation(p1).setMessage("m5").build();
+        RouteNote n6 = RouteNote.newBuilder().setLocation(p1).setMessage("m6").build();
+        int timesOnNext = 0;
+
+        // Cast of generics output error that "unchecked". This is not inevitable.
+        // Generate mock which instanciate RouteNote casted Streamobserver class.
+        @SuppressWarnings("unchecked")
+        StreamObserver<RouteNote> responseObserver = (StreamObserver<RouteNote>) mock(StreamObserver.class);
+        GreeterGrpc.GreeterStub stub = GreeterGrpc.newStub(inProcessChannel);
+
+        StreamObserver<RouteNote> requestObserver = stub.routeChat(responseObserver);
+        // Whatever argument can be allowed to set on onNext.
+        // never test method 'onNext' isn't called once.
+        verify(responseObserver, never()).onNext(any(RouteNote.class));
+
+        requestObserver.onNext(n1);
+        verify(responseObserver, never()).onNext(any(RouteNote.class));
+
+        requestObserver.onNext(n2);
+        verify(responseObserver, never()).onNext(any(RouteNote.class));
+
+        requestObserver.onNext(n3);
+        ArgumentCaptor<RouteNote> routeNoteCaptor = ArgumentCaptor.forClass(RouteNote.class);
+        verify(responseObserver,
+                timeout(100).times(++timesOnNext)).onNext(routeNoteCaptor.capture());
+        RouteNote result = routeNoteCaptor.getValue();
+        assertEquals(p1, result.getLocation());
+        assertEquals("m1", result.getMessage());
+
+        requestObserver.onCompleted();
     }
 }
