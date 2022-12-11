1b91d859dfe203ba62ee7c3a6fee75ffdb2eedd3
shotamishima <sm4mma@gmail.com>
2022-12-11 23:42:40 +0900

    Add initialization for routechat simpletest

--- src/main/java/in/tutorial/grpc/RouteGuideClient.java
+++ src/main/java/in/tutorial/grpc/RouteGuideClient.java
@@ -1,5 +1,6 @@
 package in.tutorial.grpc;
 
+
 public class RouteGuideClient {
 
 }
--- src/test/java/in/tutorial/grpc/RouteGuideClientTest.java
+++ src/test/java/in/tutorial/grpc/RouteGuideClientTest.java
@@ -1,5 +1,35 @@
 package in.tutorial.grpc;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+import io.grpc.stub.StreamObserver;
+
+@RunWith(JUnit4.class)
 public class RouteGuideClientTest {
-    
+
+    @Test
+    public void routeChat_simpleResponse() throws InterruptedException {
+        RouteNote fakeResponse1 = RouteNote.newBuilder().setMessage("dummy msg1").build();
+        RouteNote fakeResponse2 = RouteNote.newBuilder().setMessage("dummy msg2").build();
+
+        final List<String> messagesDelivered = new ArrayList<>();
+        final List<Point> locationsDelivered = new ArrayList<>();
+        final AtomicReference<StreamObserver<RouteNote>> responseObserverRef = new AtomicReference<StreamObserver<RouteNote>>();
+        // When CountDownLatch is initialized with argument 1, behavior as latch of
+        // on/off, to wait all thread call countDown() method.
+        final CountDownLatch allRequestsDelivered = new CountDownLatch(1);
+
+        assertTrue(allRequestsDelivered.await(1, TimeUnit.SECONDS));
+
+    }
 }
