37f2451d6f5eb515ee0b2b3731677e644b2677d3
shotamishima <sm4mma@gmail.com>
2022-12-14 14:50:57 +0900

    Make setup related method no error

--- src/main/java/in/tutorial/grpc/RouteGuideClient.java
+++ src/main/java/in/tutorial/grpc/RouteGuideClient.java
@@ -1,6 +1,36 @@
 package in.tutorial.grpc;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.protobuf.Message;
+
+import io.grpc.Channel;
 
 public class RouteGuideClient {
 
+    private TestHelper testHelper;
+
+    public RouteGuideClient(Channel channel) {
+
+    }
+
+    /*
+     * Only used for helping unit test.
+     */
+    interface TestHelper {
+        /*
+         * Used for verify/inspect message received from server.
+         */
+        void onMessage(Message message);
+
+        /*
+         * Used for verify/inspect error received from server.
+         */
+        void onRpcError(Throwable exception);
+    }
+
+    @VisibleForTesting
+    public void setTestHelper(TestHelper testHelper) {
+        this.testHelper = testHelper;
+
+    }
 }
--- src/test/java/in/tutorial/grpc/RouteGuideClientTest.java
+++ src/test/java/in/tutorial/grpc/RouteGuideClientTest.java
@@ -7,8 +7,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import in.tutorial.grpc.GreeterGrpc.GreeterImplBase;
+import in.tutorial.grpc.RouteGuideClient.TestHelper;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,9 +18,11 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
+import io.grpc.inprocess.InProcessChannelBuilder;
 import io.grpc.inprocess.InProcessServerBuilder;
 import io.grpc.stub.StreamObserver;
 import io.grpc.testing.GrpcCleanupRule;
+import io.grpc.util.MutableHandlerRegistry;
 
 @RunWith(JUnit4.class)
 public class RouteGuideClientTest {
@@ -26,12 +30,20 @@ public class RouteGuideClientTest {
     @Rule
     public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
 
+    private final MutableHandlerRegistry serviceRegistry = new MutableHandlerRegistry();
+    private TestHelper testHelper = mock(TestHelper.class);
+    private RouteGuideClient client;
+
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
         String serverName = InProcessServerBuilder.generateName();
-        // TODO serviceRegistry
+        // Use a mutable service registry for later registering the service impl for
+        // each test case.
         grpcCleanup.register(InProcessServerBuilder.forName(serverName).fallbackHandlerRegistry(serviceRegistry)
                 .directExecutor().build().start());
+        client = new RouteGuideClient(
+                grpcCleanup.register(InProcessChannelBuilder.forName(serverName).directExecutor().build()));
+        client.setTestHelper(testHelper);
     }
 
     @Test
